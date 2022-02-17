d0b49350a072abf0cee362ef9b45a991dab072d9
prateek41 <prateekbhagtani01@gmail.com>
2022-02-17 19:47:08 +0530

    message

---
+++ shop/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name="ShopHome"),
+    path('about/', views.about,name="About Us"),
+    path('contact/', views.contact,name="Contact Us"),
+    path('tracker/', views.tracker,name="Tracking Status"),
+    path('search/', views.search,name="Search"),
+    path('products/<int:myid>', views.productView,name="Search"),
+    path('checkout/', views.checkout,name="Checkout"),
+    path('calc/', views.calc,name="Calculate")
+]
